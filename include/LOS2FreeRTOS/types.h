d45e2d7440ab940b5dbce33618ad857ff96a20c1
itlantu <it_lantu@outlook.com>
2025-10-27 22:57:20 +0800

    feat: 首次提交, 定义了liteos中常用的类型和返回值宏

---
+++ include/LOS2FreeRTOS/types.h
@@ -0,0 +1,12 @@
+#ifndef LOS2FREERTOS_TYPES_H
+#define LOS2FREERTOS_TYPES_H
+
+#include <stdint.h>
+
+typedef unsigned int UINTPTR;
+typedef uint32_t UINT32;
+
+#define LOS_OK	0U
+#define LOS_NOK (uint32_t)(-1)
+
+#endif // LOS2FREERTOS_TYPES_H

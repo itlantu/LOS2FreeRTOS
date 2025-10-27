68e7e426f0f58961ada4d139ac3fbba84854ca53
itlantu <it_lantu@outlook.com>
2025-10-27 22:56:28 +0800

    feat: 首次提交, 定义了liteos的LOS_MS2Tick和初始化相关的函数

---
+++ include/LOS2FreeRTOS/los2freertos.h
@@ -0,0 +1,16 @@
+#ifndef LOS2FREERTOS_H
+#define LOS2FREERTOS_H
+
+#include "FreeRTOS.h"
+#include "LOS2FreeRTOS/task.h"
+
+// extern UINT32 LOS_MS2Tick(UINT32 millisec);
+uint32_t LOS_MS2Tick(const uint32_t milli_second);
+
+// extern UINT32 LOS_Start(VOID)
+uint32_t LOS_Start();
+
+// extern UINT32 LOS_KernelInit(VOID)
+uint32_t LOS_KernelInit();
+
+#endif // LOS2FREERTOS_H

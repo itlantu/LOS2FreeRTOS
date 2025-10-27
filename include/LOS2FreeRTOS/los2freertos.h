dafafbc60197cf6696f99666881dd70bb567bf8d
itlantu <it_lantu@outlook.com>
2025-10-28 01:03:36 +0800

    feat: 移除了include task.h; 新增了include types.h

--- include/LOS2FreeRTOS/los2freertos.h
+++ include/LOS2FreeRTOS/los2freertos.h
@@ -2,7 +2,7 @@
 #define LOS2FREERTOS_H
 
 #include "FreeRTOS.h"
-#include "LOS2FreeRTOS/task.h"
+#include "LOS2FreeRTOS/types.h"
 
 // extern UINT32 LOS_MS2Tick(UINT32 millisec);
 uint32_t LOS_MS2Tick(const uint32_t milli_second);
